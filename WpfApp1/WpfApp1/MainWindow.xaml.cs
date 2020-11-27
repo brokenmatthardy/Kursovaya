0cd09e4de7ddba5d5a54684885dec50d39b1a0a0
brokenmatthardy <75065741+brokenmatthardy@users.noreply.github.com>
2020-11-27 19:35:03 +0700

    Попытка добавить несколько цветов

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -51,17 +51,27 @@ namespace WpfApp1
 
                     Image img = new Image();
 
-                    if ((i == 5) && (j == 5))
+                    if ((i == 5) && (j == 3))
                     {
                         img.Source = blue;
                     }
-                    else
+                    else if ((i == 2) && (j == 4))
                     {
                         img.Source = purple;
 
                     }
-                    
-                    
+                    else if ((i == 3) && (j == 2))
+                    {
+                        img.Source = green;
+
+                    }
+                    else if ((i == 4) && (j == 5))
+                    {
+                        img.Source = orange;
+
+                    }
+
+
                     StackPanel stackPnl = new StackPanel();
                     stackPnl.Margin = new Thickness(1);
                     stackPnl.Children.Add(img);
